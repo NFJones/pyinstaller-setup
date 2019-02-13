0abbf2556040afa558081c6548dc222b806c9f07
Neil Jones <neil.franklin.jones@gmail.com>
2019-02-12 23:52:59 -0800

    Moved pyinstaller_setuptools.py into a package directory.

--- setup.py
+++ setup.py
@@ -4,32 +4,33 @@ from io import open
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
+
 setup(
-    name='pyinstaller_setuptools',
-    version='2019.0',
-    description='Use pyinstaller in your setup.py',
+    name="pyinstaller_setuptools",
+    version="2019.0",
+    description="Use pyinstaller in your setup.py",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/pypa/sampleproject',
-    author='Neil F Jones',
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    author="Neil F Jones",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    keywords='sample setuptools development pyinstaller',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
-    install_requires=['pyinstaller'],
+    keywords="sample setuptools development pyinstaller",
+    packages=["pyinstaller_setuptools"],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4",
+    install_requires=["pyinstaller"],
 )
